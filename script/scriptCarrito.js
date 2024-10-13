bd9a0c305a03fab3fb6bea2849d96b39552d3208
Juan8516 <juan.ind85@gmail.com>
2024-10-13 16:00:42 -0500

    refactor: se reemplaza proyecto para mejora de responsive y diseno

--- script/scriptCarrito.js
+++
@@ -1,6 +0,0 @@
-//Aleta boton envio carrito
-const envioProductos = document.getElementById('boton-envio');
-
-envioProductos.addEventListener('click', function() {
-    alert('Pedido enviado correctamente');
-});
--- script/scriptContacto.js
+++
@@ -1,6 +0,0 @@
-//Aleta boton contacto
-const buttonContacto = document.getElementById('buttonContacto');
-
-buttonContacto.addEventListener('click', function() {
-    alert('Informacion recibida, contactaremos lo antes posible.');
-});
